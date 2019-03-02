97f4d915927cc56492b7a5c4e807a64f09d8772a
AlexisAG <thedeath475@gmail.com>
2019-03-02 18:48:55 -0500

    Update MainMenu.cs

--- GameJam Mars Uqac/Assets/Scripts/MainMenu.cs
+++ GameJam Mars Uqac/Assets/Scripts/MainMenu.cs
@@ -24,7 +24,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("Map", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Map", LoadSceneMode.Single);
     }
 
     public void GoToInstructions(bool value)
