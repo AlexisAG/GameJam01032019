92f5b806862aa2a2fc6ae66335031dd04e9cf8b9
Thibault DUFOUR <t.dufour740@gmail.com>
2019-03-03 12:13:47 -0500

    Mine explosion + trail

--- GameJam Mars Uqac/Assets/Scripts/Mine.cs
+++ GameJam Mars Uqac/Assets/Scripts/Mine.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class Mine : MonoBehaviour
 {
     public string m_PlayerTag;
+    public GameObject m_ExplosionEffect;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void MakeExplosionEffect()
+    {
+        Instantiate<GameObject>(m_ExplosionEffect, transform);
+    }
+
+    void OnDestroy()
+    {
+
     }
 
 }
