02cca257086997ee614000283276618a32398778
AlexisAG <agay@amiltone.com>
2021-04-10 23:46:27 +0200

    Create Instruction GameState + update controller

---
+++ RedDeadOxygen/Assets/GameMode/MainMenu/GameStates/InstructionGameState.cs
@@ -0,0 +1,19 @@
+using AgToolkit.Core.GameModes.GameStates;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class InstructionData : IGameStateData
+{
+    [SerializeField]
+    private GameObject _ui;
+
+    public GameObject Ui => _ui;
+}
+
+public class InstructionGameState : GameStateMonoBehaviour<InstructionData>
+{
+
+}
--- RedDeadOxygen/Assets/GameMode/MainMenu/MainMenuGameMode.cs
+++ RedDeadOxygen/Assets/GameMode/MainMenu/MainMenuGameMode.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class MainMenuGameMode : GameMode
 {
     [SerializeField]
+    private string _instructionTrigger;
+    [SerializeField]
     private string _soloTrigger;
     [SerializeField]
     private string _multiTrigger;
 
+    public string InstructionTrigger => _instructionTrigger;
     public string SoloTrigger => _soloTrigger;
     public string MultiTrigger => _multiTrigger;
 }
