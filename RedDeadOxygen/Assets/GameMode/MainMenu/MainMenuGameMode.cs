bc830217ae8ae5ac2e8f4a516a17d88c64e1fa58
AlexisAG <agay@amiltone.com>
2021-04-11 15:55:48 +0200

    GameMode triggers + GameMode controller

--- RedDeadOxygen/Assets/GameMode/MainMenu/MainMenuGameMode.cs
+++ RedDeadOxygen/Assets/GameMode/MainMenu/MainMenuGameMode.cs
@@ -6,13 +6,22 @@ using UnityEngine;
 public class MainMenuGameMode : GameMode
 {
     [SerializeField]
+    private string _mainMenuTrigger;
+    [SerializeField]
     private string _instructionTrigger;
+    [SerializeField]
+    private string _playerOptionTrigger;
+    [SerializeField]
+    private string _gameOptionTrigger;
     [SerializeField]
     private string _soloTrigger;
     [SerializeField]
     private string _multiTrigger;
 
+    public string MainMenuTrigger => _mainMenuTrigger;
     public string InstructionTrigger => _instructionTrigger;
+    public string PlayerOptionTrigger => _playerOptionTrigger;
+    public string GameOptionTrigger => _gameOptionTrigger;
     public string SoloTrigger => _soloTrigger;
     public string MultiTrigger => _multiTrigger;
 }
