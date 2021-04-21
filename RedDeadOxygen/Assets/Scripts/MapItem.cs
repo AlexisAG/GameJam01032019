88c8a4ded2102550d814de1db9dfe8ad64b4e3b1
AlexisAG <agay@amiltone.com>
2021-04-21 18:20:38 +0200

    New UI prefabs + script

---
+++ RedDeadOxygen/Assets/Scripts/MapItem.cs
@@ -0,0 +1,11 @@
+﻿using AgToolkit.AgToolkit.Core.GameModes;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "RDO/MapItem", fileName = "NewMapItem")]
+public class MapItem : ScriptableObject
+{
+    [SerializeField]
+    private EnumGameMode _map;
+
+    public EnumGameMode Map => _map;
+}
