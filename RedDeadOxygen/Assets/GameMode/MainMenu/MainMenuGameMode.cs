9089f35a08806208525c1082a4e0f789537a1df4
AlexisAG <thedeath475@gmail.com>
2021-05-16 18:13:51 +0200

    Revert "GameMode triggers + GameMode controller"
    This reverts commit 15718e1bc34516c6ddf5469bb2a2798e8914f606.

--- RedDeadOxygen/Assets/GameMode/MainMenu/MainMenuGameMode.cs
+++ RedDeadOxygen/Assets/GameMode/MainMenu/MainMenuGameMode.cs
@@ -6,22 +6,13 @@ using UnityEngine;
 public class MainMenuGameMode : GameMode
 {
     [SerializeField]
-    private string _mainMenuTrigger;
-    [SerializeField]
     private string _instructionTrigger;
-    [SerializeField]
-    private string _playerOptionTrigger;
-    [SerializeField]
-    private string _gameOptionTrigger;
     [SerializeField]
     private string _soloTrigger;
     [SerializeField]
     private string _multiTrigger;
 
-    public string MainMenuTrigger => _mainMenuTrigger;
     public string InstructionTrigger => _instructionTrigger;
-    public string PlayerOptionTrigger => _playerOptionTrigger;
-    public string GameOptionTrigger => _gameOptionTrigger;
     public string SoloTrigger => _soloTrigger;
     public string MultiTrigger => _multiTrigger;
 }
