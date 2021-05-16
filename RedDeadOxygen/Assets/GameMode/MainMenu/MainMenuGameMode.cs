002d81871ba78fd75f769e5173b838ec3787ef94
AlexisAG <thedeath475@gmail.com>
2021-05-16 18:13:51 +0200

    Revert "Create Instruction GameState + update controller"
    This reverts commit 02cca257086997ee614000283276618a32398778.

--- RedDeadOxygen/Assets/GameMode/MainMenu/MainMenuGameMode.cs
+++ RedDeadOxygen/Assets/GameMode/MainMenu/MainMenuGameMode.cs
@@ -6,13 +6,10 @@ using UnityEngine;
 public class MainMenuGameMode : GameMode
 {
     [SerializeField]
-    private string _instructionTrigger;
-    [SerializeField]
     private string _soloTrigger;
     [SerializeField]
     private string _multiTrigger;
 
-    public string InstructionTrigger => _instructionTrigger;
     public string SoloTrigger => _soloTrigger;
     public string MultiTrigger => _multiTrigger;
 }
--- RedDeadOxygen/Assets/Scripts/MainMenu.cs
+++ RedDeadOxygen/Assets/Scripts/MainMenu.cs
@@ -7,38 +7,34 @@ using AgToolkit.Core.GameModes;
 
 public class MainMenu : MonoBehaviour
 {
-    private MainMenuGameMode _gameMode;
 
     public Canvas m_mainMenuCanvas;
+    public Canvas m_instructionsCanvas;
     public Button m_Play, m_Back;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
-        _gameMode = GameManager.Instance.GetCurrentGameMode<MainMenuGameMode>();
         m_Play.Select();
     }
 
     public void StartGame()
     {
-        _gameMode?.GetComponent<Animator>()?.SetTrigger(_gameMode.SoloTrigger);
+        Time.timeScale = 1;
+        MainMenuGameMode gameMode = GameManager.Instance.GetCurrentGameMode<MainMenuGameMode>();
+        gameMode?.GetComponent<Animator>()?.SetTrigger(gameMode.SoloTrigger);
     }
 
     public void GoToInstructions(bool value)
     {
+        m_instructionsCanvas.gameObject.SetActive(value);
         m_mainMenuCanvas.gameObject.SetActive(!value);
 
         if (value)
-        {
-            _gameMode?.GetComponent<Animator>()?.SetTrigger(_gameMode.InstructionTrigger);
             m_Back.Select();
-        }
         else
-        {
-
             m_Play.Select();
-        }
 
     }
 }
